3c269c5b044effe14fabf2d0307ec41603c0cb17
haywirebanana <113632239+haywirebanana@users.noreply.github.com>
2025-04-29 01:37:03 -0600

    fix: remove space between pipe and backslash within install_terraform

--- scripts/ubuntu_dev_env_setup.sh
+++ scripts/ubuntu_dev_env_setup.sh
@@ -79,8 +79,8 @@ install_google_cloud_sdk() {
 install_terraform() {
     echo -e "${YELLOW}Installing Terraform...${RESET}"
 
-    wget -O- https://apt.releases.hashicorp.com/gpg | \ 
-    gpg --dearmor | \
+    wget -O- https://apt.releases.hashicorp.com/gpg |\ 
+    gpg --dearmor |\
     sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
