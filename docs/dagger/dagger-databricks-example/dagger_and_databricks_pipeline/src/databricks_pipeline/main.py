a609b942c53f214daf415902ca98e6742640a88f
Schiiss <conner.schiissler@gmail.com>
2025-04-29 05:26:29 -0600

    started adding secrets to container

--- docs/dagger/dagger-databricks-example/dagger_and_databricks_pipeline/src/databricks_pipeline/main.py
+++ docs/dagger/dagger-databricks-example/dagger_and_databricks_pipeline/src/databricks_pipeline/main.py
@@ -5,14 +5,23 @@ from dagger import dag, function, object_type
 @object_type
 class DatabricksPipeline:
     @function
-    def databricks_cli_container(self) -> dagger.Container:
+    def databricks_cli_container(
+        self, 
+        directory_arg: dagger.Directory, 
+        databricks_workspace_url: dagger.Secret,
+        ) -> dagger.Container:
         """Creates an Alpine container with Databricks CLI installed via curl installer"""
         return (
             dag.container()
             .from_("alpine:latest")
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_workdir("/mnt")
+            .with_secret_variable("DATABRICKS_HOST", databricks_workspace_url)
+            .with_secret_variable("ARM_CLIENT_SECRET", client_secret)
             .with_exec(["apk", "add", "--no-cache", "bash", "curl"])
             .with_exec([
                 "sh", "-c",
                 "curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sh"
             ])
+            .with_exec(["databricks", "bundle", "deploy", "-t", "dev"])
         )
