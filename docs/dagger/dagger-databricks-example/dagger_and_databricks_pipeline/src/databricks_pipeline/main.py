e2403fbf73aa9c398af374d06af7e9512c3975f3
Schiiss <conner.schiissler@gmail.com>
2025-04-30 16:27:19 -0600

    added ability to call unit tests

--- docs/dagger/dagger-databricks-example/dagger_and_databricks_pipeline/src/databricks_pipeline/main.py
+++ docs/dagger/dagger-databricks-example/dagger_and_databricks_pipeline/src/databricks_pipeline/main.py
@@ -12,7 +12,7 @@ class DatabricksPipeline:
         databricks_client_secret: dagger.Secret,
         databricks_client_id: dagger.Secret,
     ) -> dagger.Container:
-        """Creates an Alpine container with Databricks CLI configured for OAuth M2M authentication"""
+        """Creates an Alpine container with Databricks CLI configured for OAuth M2M authentication. Deploys an asset bundle."""
 
         profile_name = "dagger_oauth_profile"
         config_path = "/root/.databrickscfg"
@@ -40,6 +40,44 @@ class DatabricksPipeline:
                 """
             ])
             .with_exec(["databricks", "bundle", "deploy", "-t", "dev", "--profile", profile_name])
+        )
+    
+    @function
+    def databricks_workflow_run(
+        self,
+        directory_arg: dagger.Directory,
+        databricks_workspace_url: dagger.Secret,
+        databricks_client_secret: dagger.Secret,
+        databricks_client_id: dagger.Secret,
+    ) -> dagger.Container:
+        """Creates an Alpine container with Databricks CLI configured for OAuth M2M authentication. Runs an asset bundle."""
+        
+        profile_name = "dagger_oauth_profile"
+        config_path = "/root/.databrickscfg"
+
+        return (
+            dag.container()
+            .from_("alpine:latest")
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_workdir("/mnt")
+            .with_exec(["apk", "add", "--no-cache", "bash", "curl"])
+            .with_exec([
+                "sh", "-c",
+                "curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sh"
+            ])
+            .with_secret_variable("DATABRICKS_HOST", databricks_workspace_url)
+            .with_secret_variable("DATABRICKS_CLIENT_SECRET", databricks_client_secret)
+            .with_secret_variable("DATABRICKS_CLIENT_ID", databricks_client_id)
+            .with_exec([
+                "sh", "-c",
+                f"""
+                echo '[{profile_name}]' > {config_path} && \
+                echo "host = $DATABRICKS_HOST" >> {config_path} && \
+                echo "client_id = $DATABRICKS_CLIENT_ID" >> {config_path} && \
+                echo "client_secret = $DATABRICKS_CLIENT_SECRET" >> {config_path}
+                """
+            ])
+            .with_exec(["databricks", "bundle", "run", "-t", "dev", "dagger_dbricks_job_tests", "--profile", profile_name])
         )
 
 
