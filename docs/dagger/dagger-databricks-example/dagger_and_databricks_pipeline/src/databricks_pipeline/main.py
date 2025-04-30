cd5fe3533885e0c4c2d70ed61835eb8de706c408
Schiiss <conner.schiissler@gmail.com>
2025-04-30 16:44:59 -0600

    initial dagger function to create a databricks agent

--- docs/dagger/dagger-databricks-example/dagger_and_databricks_pipeline/src/databricks_pipeline/main.py
+++ docs/dagger/dagger-databricks-example/dagger_and_databricks_pipeline/src/databricks_pipeline/main.py
@@ -80,4 +80,46 @@ class DatabricksPipeline:
             .with_exec(["databricks", "bundle", "run", "-t", "dev", "dagger_dbricks_job_tests", "--profile", profile_name])
         )
 
+    @function
+    async def databricks_agent(
+        self,
+        assignment: str,
+        directory_arg: dagger.Directory,
+        databricks_workspace_url: dagger.Secret,
+        databricks_client_secret: dagger.Secret,
+        databricks_client_id: dagger.Secret,
+    ) -> str:
+        """
+        """
+        environment = (
+            dag.env()
+            .with_string_input("assignment", assignment, "the assignment to complete")
+            .with_container_input(
+                "databricks_runner",
+            dag.container()
+            .from_("alpine:latest")
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_workdir("/mnt")
+            .with_exec(["apk", "add", "--no-cache", "bash", "curl"])
+            .with_exec([
+                "sh", "-c",
+                "curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sh"
+            ])
+            .with_secret_variable("DATABRICKS_HOST", databricks_workspace_url)
+            .with_secret_variable("DATABRICKS_CLIENT_SECRET", databricks_client_secret)
+            .with_secret_variable("DATABRICKS_CLIENT_ID", databricks_client_id),
+            "a container to use for analyzing databricks artifacts"
+            )
+            .with_container_output(
+                "completed", "the completed assignment in the Databricks container"
+            )
+        )
 
+        analyze_results = (
+            dag.llm()
+            .with_env(environment)
+            .with_prompt_file(dag.current_module().source().file("databricks_prompt.txt"))
+        )
+
+        # Return the analyzed result
+        return await analyze_results.last_reply()
